877c5d528d4725fab20bf733de9d69d082de2fe9
胡一凡 <549804563@qq.com>
2020-11-02 10:21:07 +0800

    test

--- webpack.config.js
+++ webpack.config.js
@@ -7,17 +7,8 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 module.exports = {
     // entry: './src/index.js',
     entry: {
-        // app: './src/index.js',
-        // print: './src/print.js'
         index: './src/index.js',
-        // another: './src/another-module.js'
     },
-    
-    // devtool: 'inline-source-map',
-    // devServer: {
-    //     contentBase: './dist',
-    //     hot: true
-    // },
     optimization: {
         splitChunks: {
             cacheGroups: {
@@ -33,61 +24,14 @@ module.exports = {
             }
         }
     },
-    // optimization: {
-    //     splitChunks: {
-    //         cacheGroups: {
-    //             commons: {
-    //                 name: 'commons',
-    //                 chunks: 'initial',
-    //                 minChunks: 2
-    //             }
-    //         }
-    //     }
-    // },
     plugins: [
         new HtmlWebpackPlugin({
             title: 'Caching',
         }),
-        // new webpack.optimize.CommonsChunkPlugin({
-        //     name: 'manifest'
-        // }),
         new CleanWebpackPlugin()
     ],
     output: {
-        // filename: '[name].bundle.js',
         filename: '[name].[chunkhash].js',
         path: path.resolve(__dirname, 'dist')
-    },
-    // mode: "production",
-    // module: {
-    //     rules: [
-    //         {
-    //             test: /\.css$/,
-    //             use: ['style-loader', 'css-loader']
-    //         }
-    //     ]
-    // }
-    // module: {
-    //     rules: [
-    //         {
-    //             test: /\.css$/,
-    //             use: [
-    //                 'style-loader',
-    //                 'css-loader'
-    //             ]
-    //         },
-    //         {
-    //             test: /\.(png|svg|jpg|gif)$/,
-    //             use: [
-    //                 'file-loader'
-    //             ]
-    //         },
-    //         {
-    //             test: /\.xml$/,
-    //             use: [
-    //                 'xml-loader'
-    //             ]
-    //         }
-    //     ]
-    // }
+    }
 }
